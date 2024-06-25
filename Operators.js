454f0da056aba1f9959f230ede025d8b7d99efb5
RSHCM2021/tuanna <tuanna9@runsystem.net>
2024-06-25 19:07:50 +0900

    commit change

---
+++ Operators.js
@@ -0,0 +1,29 @@
+/*
+let a = 5;
+let b = 2;
+let c = a**b; // mũ
+console.log(c);
+*/
+
+/*
+let a = 10/3   // chia lấy phần nguyên
+let b = 10%3 // chia lấy phần dư
+console.log(a)
+console.log(b)
+*/
+
+/*
+let a = 1;
+console.log(a++); // thực hiện dòng lệnh hiện tại trước rồi sau đó mới cộng vào biến đó, tương tự cho phép trừ
+console.log(a);
+*/
+
+/*
+let a = 1
+console.log(++a); // cộng biến vào trước rồi mới thục hiện dòng lệnh, tương tự cho phép trừ
+console.log(a);
+*/
+
+let a = 3;
+let b = 45;
+a += b; // tương ứng a=a+b
