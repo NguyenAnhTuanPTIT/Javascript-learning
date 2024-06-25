d09e391d503a8cc961903a908d3e5527c0050b9d
RSHCM2021/tuanna <tuanna9@runsystem.net>
2024-06-25 18:32:44 +0900

    commit

--- test.js
+++ test.js
@@ -1,1 +1,48 @@
-abc
+/* Show the ouput in the console log 
+console.log("Hello world")
+*/
+
+/*
+let a = 5;
+let b = 4
+let c = a +b;
+console.log(c);
+*/
+
+/*{
+    let a = 3
+    {
+        console.log(a)    
+    }
+
+}
+console.log(a)*/
+
+/*
+let a = null;
+console.log(a)
+*/
+
+/*
+{
+    var a = 7;
+}
+console.log(a)
+*/
+//=> Var là khai báo biến toàn cục, let là khai báo cục bộ
+//=> Biến dùng var có thể thay đổi giá trị được, còn biến dùng let không thay đổi giá trị được
+//=> Nếu khai báo biến không dùng var hay let (vd: x = 3) thì được coi là biến toàn cục
+
+/*
+const PI = 3.14
+
+PI = 3;
+*/
+
+/*
+const objA = {};
+console.log(objA);
+objA ["name"]= "Nguyen Anh Tuan";
+console.log(objA);
+*/
+
